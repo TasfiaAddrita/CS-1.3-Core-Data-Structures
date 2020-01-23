6ab2baf07e547b4cec847b01fe2c3fedd966eaef
TasfiaAddrita <tasfia.addrita@gmail.com>
2020-01-23 13:49:45 -0800

    completed convert function; completed bases.py and pass all tests

--- Code/bases.py
+++ Code/bases.py
@@ -95,15 +95,12 @@ def convert(digits, base1, base2):
     # Handle up to base 36 [0-9a-z]
     assert 2 <= base1 <= 36, 'base1 is out of range: {}'.format(base1)
     assert 2 <= base2 <= 36, 'base2 is out of range: {}'.format(base2)
-    # TODO: Convert digits from base 2 to base 16 (and vice versa)
-    # ...
-    # TODO: Convert digits from base 2 to base 10 (and vice versa)
-    # ...
-    # TODO: Convert digits from base 10 to base 16 (and vice versa)
-    # ...
-    # TODO: Convert digits from any base to any base (2 up to 36)
-    # ...
-
+    conversion = None
+    # Convert digits from base 2 to base 16 (and vice versa)
+    # Convert digits from base 2 to base 10 (and vice versa)
+    # Convert digits from base 10 to base 16 (and vice versa)
+    # Convert digits from any base to any base (2 up to 36)
+    return encode(decode(digits, base1), base2)
 
 def main():
     """Read command-line arguments and convert given digits between bases."""
